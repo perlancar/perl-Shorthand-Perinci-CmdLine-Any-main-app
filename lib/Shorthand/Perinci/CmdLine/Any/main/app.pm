817405441ab5b11d6102006c32181e947a65e71d
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-02-23 00:23:10 +0700

    Release v0.001

--- lib/Shorthand/Perinci/CmdLine/Any/main/app.pm
+++ lib/Shorthand/Perinci/CmdLine/Any/main/app.pm
@@ -3,6 +3,9 @@ package Shorthand::Perinci::CmdLine::Any::main::app;
 # DATE
 # VERSION
 
+use strict;
+use warnings;
+
 use Perinci::CmdLine::Any;
 
 my %extra_args;
@@ -12,9 +15,39 @@ sub import {
     %extra_args = @_;
 }
 
-INIT {
-    Perinci::CmdLine::Any->new(
-        url => '/main/app',
-        %extra_args,
-    )->run;
+END {
+    no warnings 'once';
+    if ($main::SPEC{app}) {
+        Perinci::CmdLine::Any->new(
+            url => '/main/app',
+            %extra_args,
+        )->run;
+    }
 }
+
+1;
+# ABSTRACT: Load Perinci::CmdLine::Any and run main's app
+
+=head1 SYNOPSIS
+
+ use Shorthand::Perinci::CmdLine::Any::main::app;
+
+=head1 DESCRIPTION
+
+B<EXPERIMENTAL.>
+
+ use Shorthand::Perinci::CmdLine::Any::main::app;
+
+is a shorthand for:
+
+ use Perinci::CmdLine::Any;
+ END { Perinci::CmdLine::Any->new(url => '/main/app')->run; }
+
+You can pass extra arguments to new() via import arguments, e.g.:
+
+ use Shorthand::Perinci::CmdLine::Any::main::app log=>1;
+
+is a shorthand for:
+
+ use Perinci::CmdLine::Any;
+ END { Perinci::CmdLine::Any->new(url => '/main/app', log=>1)->run; }
